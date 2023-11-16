7703ab2c49562acd7600ebaa2aff2b678df6d651
doee-hc <1316096865@qq.com>
2023-11-16 22:58:09 +0800

    Fix PE distribution bug

--- dict_analyze.py
+++ dict_analyze.py
@@ -42,11 +42,26 @@ def nonzero_indices(arr, axis, index):
     else:
         raise ValueError("Invalid axis. Use 'row' or 'col'.")
 
+def check_one_nonzero(arr):
+    # 检查每一行非零元素的数量
+    nonzero_count = np.count_nonzero(arr, axis=1)
+    
+    # 检查每一行是否只有一个非零元素
+    all_one_nonzero = np.all(nonzero_count == 1)
+    
+    return all_one_nonzero
 
 def find_u_v_max_path(u,v,w):
     res_mul_num = []
     res_add_num = []
     # 搜索每条res的路径
+    #for col_i in range(u.shape[1]):
+    #    u_add_num = count_nonzero(w, 'row', i) - 1
+    #    v_add_num = count_nonzero(v, 'row', i) - 1
+    #    u_v_add_num.append(u_add_num + v_add_num)
+    #u_v_add_max_index = np.argmax(u_v_add_num)
+
+
     for i in range(w.shape[0]):
         mul_num = 0
         add_num = count_nonzero(w, 'row', i) - 1 
@@ -68,7 +83,16 @@ def find_u_v_max_path(u,v,w):
     max_path = sorted_indices[0]
     path_num_max = res_mul_num[max_path] + res_add_num[max_path]
 
-    return (path_num_max,max_path)
+    max_u_v_path = 0
+    max_u_v_add_num = 0
+    for i in range(w.shape[1]):
+        if w[max_path,i] != 0:
+            # 查找res的相关数目
+            add_num = count_nonzero(u, 'col', i) + count_nonzero(v, 'col', i) 
+            if add_num > max_u_v_add_num:
+                max_u_v_path = i
+
+    return (path_num_max,max_u_v_path)
 
 def find_w_max_path(arr):
     non_zero_counts = np.count_nonzero(arr, axis=1)
@@ -123,15 +147,15 @@ def find_most_significant_adder(arr,arr_coe,index,axis):
 
 class PE:
     pe_counter = 2  # 类变量，用于跟踪PE的编号
-    def __init__(self,index,level,typeofPE,in0,in1,pe_number0,pe_number1):
+    def __init__(self,index,level,typeofPE,in0,in1,pe_number0,pe_number1,coe0,coe1):
         self.index = index
         self.level = level
         self.in0 = in0
         self.in1 = in1
         self.in0_pe_num = pe_number0
         self.in1_pe_num = pe_number1
-        self.coe0 = 0.0
-        self.coe1 = 0.0
+        self.coe0 = coe0
+        self.coe1 = coe1
         i0 = ''
         i1 = ''
         if typeofPE == 'u_add':
@@ -168,7 +192,7 @@ class PE:
 
 
 
-def distribute_pe(u,v,w,w_act,index,arch,level,typeofPE,in0,in1):
+def distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,index,arch,level,typeofPE,in0,in1):
     # 如果level不存在，创建一个空列表
     if level not in arch:
         arch[level] = []
@@ -177,29 +201,33 @@ def distribute_pe(u,v,w,w_act,index,arch,level,typeofPE,in0,in1):
     if typeofPE == 'u_add':
         pe_number0 = u[in0][index]
         pe_number1 = u[in1][index]
-
+        pe_coe0 = u_coe[in0][index]
+        pe_coe1 = u_coe[in1][index]
     # v的加法
     if typeofPE == 'v_add':
         pe_number0 = v[in0][index]
         pe_number1 = v[in1][index]
-
+        pe_coe0 = v_coe[in0][index]
+        pe_coe1 = v_coe[in1][index]
     # 乘法
     if typeofPE == 'mul':
-
         pe_number0 = u[in0][index]
         pe_number1 = v[in1][index]
-
+        pe_coe0 = u_coe[in0][index]
+        pe_coe1 = v_coe[in1][index]
     # w的加法
     if typeofPE == 'w_add':
         pe_number0 = w_act[index][in0]
         pe_number1 = w_act[index][in1]
-
+        pe_coe0 = w_coe[index][in0]
+        pe_coe1 = w_coe[index][in1]
 
     # 创建PE
-    new_pe = PE(index,level,typeofPE,in0,in1,pe_number0,pe_number1)
+    new_pe = PE(index,level,typeofPE,in0,in1,pe_number0,pe_number1,pe_coe0,pe_coe1)
+
 
     # 检查新创建的 PE 对象的 name 是否已经存在于系统结构中
-    name_exists = any(pe.name == new_pe.name for pe in arch[level])
+    name_exists = any((pe.name == new_pe.name and (pe.coe0/new_pe.coe0 == pe.coe1/new_pe.coe1)) for pe in arch[level])
 
     if name_exists :
         PE.pe_counter -= 1
@@ -225,6 +253,9 @@ def coefficient_backup(u,v,w,u_coe,v_coe,w_coe):
             w_coe[i][j] = w[i][j]
             if w[i][j] != 0 :
                 w[i][j] = 1 
+    u = u.astype(int)
+    v = v.astype(int)
+    w = w.astype(int)
 
 
 # 每次level分配完pe，刷新一下矩阵，用于下个level分配
@@ -271,12 +302,13 @@ def refresh_calculation(u,v,w,u_coe,v_coe,w_coe,w_act,arch,level):
                         u_coe[in0][i] = ratio0
                         u_coe[in1][i] = 0
             # 最后更新 index 位置
-            pe.coe0 = u_coe[in0][index]
-            pe.coe1 = u_coe[in1][index]
             u[in0][index] = pe.number
             u[in1][index] = 0
             u_coe[in0][index] = 1
             u_coe[in1][index] = 0
+            #print(f'refresh {pe.name}')
+            #print(f"pe number{pe.number}")
+            #print(f'u\r\n{u}')
 
 
         if pe.typeofPE == 'v_add':
@@ -302,16 +334,17 @@ def refresh_calculation(u,v,w,u_coe,v_coe,w_coe,w_act,arch,level):
                         v_coe[in0][i] = ratio0
                         v_coe[in1][i] = 0
             # 最后更新 index 位置
-            pe.coe0 = v_coe[in0][index]
-            pe.coe1 = v_coe[in1][index]
+
             v[in0][index] = pe.number
             v[in1][index] = 0
             v_coe[in0][index] = 1 
             v_coe[in1][index] = 0
+            #print(f'refresh {pe.name}')
+            #print(f"pe number{pe.number}")
+            #print(f'v\r\n{v}')
 
         if pe.typeofPE == 'mul':
-            pe.coe0 = u_coe[in0][index]
-            pe.coe1 = v_coe[in1][index]
+
             u[in0][index] = 0
             v[in1][index] = 0
             u_coe[in0][index] = 0
@@ -321,6 +354,11 @@ def refresh_calculation(u,v,w,u_coe,v_coe,w_coe,w_act,arch,level):
                 if (ratio != 0):
                     # 激活w
                     w_act[i][index] = pe.number
+            #print(f'refresh {pe.name}')
+            #print(f"pe number{pe.number}")
+            #print(f'u\r\n{u}')
+            #print(f'v\r\n{v}')
+
         if pe.typeofPE == 'w_add':
             for i in range(w.shape[0]):
                 if i != index :
@@ -341,15 +379,15 @@ def refresh_calculation(u,v,w,u_coe,v_coe,w_coe,w_act,arch,level):
                         w_coe[i][in0] = ratio0
                         w_coe[i][in1] = 0
 
-            pe.coe0 = w_coe[index][in0]
-            pe.coe1 = w_coe[index][in1]
             w_act[index][in0] = pe.number
             w_act[index][in1] = 0
             w_coe[index][in0] = 1
             w_coe[index][in1] = 0
-            print(f"level{pe.level}")
-            print(f'refresh {pe.name}')
-            print(f'w_act\r\n{w_act}')
+            #print(f"level{pe.level}")
+            #print(f"pe number{pe.number}")
+            #print(f'refresh {pe.name}')
+            #print(f'w_act\r\n{w_act}')
+            #print(f'w_coe\r\n{w_coe}')
 
 def search_pe_level(arch,target_number):
     for level,pe_list in arch.items():
@@ -430,21 +468,24 @@ def print_equations(u, v, w):
 
     coefficient_backup(u,v,w,u_coe,v_coe,w_coe)
     
-    #print(u)
-    #print(v)
-    #print(w)
+    print(u)
+    print(v)
+    print(w)
 
-    #print(u_coe)
-    #print(v_coe)
-    #print(w_coe)
+    print(u_coe)
+    print(v_coe)
+    print(w_coe)
     
     (total_level,max_path_col) = find_u_v_max_path(u,v,w)
     print(f'total_level:{total_level}')
 
-    for level_n in range(total_level):
-
-        (this_total_path,max_path_col) = find_u_v_max_path(u,v,w)
-        max_path_row = find_w_max_path(w_act)
+    level_n = 0;
+    while(check_one_nonzero(w_coe) != True):
+        print(f'level:{level_n}')
+        (this_total_path,max_path_col) = find_u_v_max_path(u,v,w_coe)
+        max_path_row = find_w_max_path(w_coe)
+        #print(f'u_v_max_col = {max_path_col}')
+        #print(f'w_max_row = {max_path_row}')
 
         u_nonzero_count = count_nonzero(u, 'col', max_path_col)
         v_nonzero_count = count_nonzero(v, 'col', max_path_col)
@@ -452,15 +493,15 @@ def print_equations(u, v, w):
 
         if(u_nonzero_count > 2):
             (u_in0,u_in1) = find_most_significant_adder(u,u_coe,max_path_col,'col')
-            distribute_pe(u,v,w,w_act,max_path_col,pe_arch,level_n,'u_add',u_in0,u_in1)
+            distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,max_path_col,pe_arch,level_n,'u_add',u_in0,u_in1)
             print(f'u most significant:{u_in0},{u_in1}')
         if(v_nonzero_count > 2):
             (v_in0,v_in1) = find_most_significant_adder(v,v_coe,max_path_col,'col')
-            distribute_pe(u,v,w,w_act,max_path_col,pe_arch,level_n,'v_add',v_in0,v_in1)
+            distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,max_path_col,pe_arch,level_n,'v_add',v_in0,v_in1)
             print(f'v most significant:{v_in0},{v_in1}')
         if(w_nonzero_count > 2):
             (w_in0,w_in1) = find_most_significant_adder(w_act,w_coe,max_path_row,'row')
-            distribute_pe(u,v,w,w_act,max_path_row,pe_arch,level_n,'w_add',w_in0,w_in1)
+            distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,max_path_row,pe_arch,level_n,'w_add',w_in0,w_in1)
             print(f'w most significant:{w_in0},{w_in1}')
 
         for i in range(u.shape[1]):
@@ -471,17 +512,17 @@ def print_equations(u, v, w):
             if (u_nonzero_count == 1) and (v_nonzero_count == 1):
                 in0 = u_nonzero_index[0]
                 in1 = v_nonzero_index[0]
-                distribute_pe(u,v,w,w_act,i,pe_arch,level_n,'mul',in0,in1)
+                distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,i,pe_arch,level_n,'mul',in0,in1)
 
             if (u_nonzero_count == 2):
                 in0 = u_nonzero_index[0]
                 in1 = u_nonzero_index[1]
-                distribute_pe(u,v,w,w_act,i,pe_arch,level_n,'u_add',in0,in1)
+                distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,i,pe_arch,level_n,'u_add',in0,in1)
 
             if (v_nonzero_count == 2):
                 in0 = v_nonzero_index[0]
                 in1 = v_nonzero_index[1]
-                distribute_pe(u,v,w,w_act,i,pe_arch,level_n,'v_add',in0,in1)
+                distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,i,pe_arch,level_n,'v_add',in0,in1)
 
         for i in range(w.shape[0]):
             w_act_nonzero_index = nonzero_indices(w_act, 'row', i) 
@@ -489,10 +530,11 @@ def print_equations(u, v, w):
             if(w_act_nonzero_count == 2):
                 in0 = w_act_nonzero_index[0]
                 in1 = w_act_nonzero_index[1]
-                distribute_pe(u,v,w,w_act,i,pe_arch,level_n,'w_add',in0,in1)
+                distribute_pe(u,v,w,u_coe,v_coe,w_coe,w_act,i,pe_arch,level_n,'w_add',in0,in1)
                 #print(f'distribute w_add:{i},{in0},{in1}')
 
         refresh_calculation(u,v,w,u_coe,v_coe,w_coe,w_act,pe_arch,level_n)
+        level_n += 1
         #print(f"u:\r\n{u}")
         #print(f"v:\r\n{v}")
         #print(f"w_act:\r\n{w_act}")
